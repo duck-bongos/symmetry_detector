fc26fb2844cb4fd5a3cf171be1fc5b28371eee03
Dan Billmann <billmannd@gmail.com>
2022-11-28 12:24:14 -0500

    Create main.cc

---
+++ src/main.cc
@@ -0,0 +1,17 @@
+/* 
+Author: Dan Billmann
+Github: duck-bongos
+
+following Google Style Docs for C++
+*/
+#include <iostream>
+#include "../include/mathUtils.h"
+#include <dlib.h>
+
+int main(){
+  int seven = 7;
+  
+  std::cout << sqrt(7) << std::endl;
+  std::cout << sq(7) << std::endl;
+  return 0;
+}
